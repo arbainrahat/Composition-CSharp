7513d912ff39338bd308b65b560bd90b5c6e4536
arbainrahat <68120270+arbainrahat@users.noreply.github.com>
2020-07-30 21:55:58 +0500

    Add files via upload

---
+++ Composition/com/mangSystem/ChemDep.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composition.com.mangSystem
+{
+    class ChemDep : Department
+    {
+       
+        public int NumberofStudents { get; set; }
+    }
+}
---
+++ Composition/com/mangSystem/University.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composition.com.mangSystem
+{
+    enum Dep
+    {
+        comp,
+        chem
+    }
+    class University
+    {
+        public List<Department> departments  = new List<Department>();
+
+        public void AddDepartment(string nam, string dnam,Dep dep,int no)
+        {
+            if (dep == Dep.chem)
+            {
+                ChemDep obj = new ChemDep();
+
+                obj.Name = nam;
+                obj.Director = dnam;
+                obj.NumberofStudents = no;
+                //We make list of base class but we add child class in list, we can do it.
+                departments.Add(obj);
+
+            }
+            else if (dep==Dep.comp)
+            {
+                CompDep obj = new CompDep();
+
+                obj.Name = nam;
+                obj.Director = dnam;
+                obj.Numberofcomp = no;
+
+                departments.Add(obj);
+
+            }
+        }
+        /*
+       public void Init_Chem(string n,string dn,int no)
+        {
+            ChemDep obj = new ChemDep();
+
+            obj.Name = n;
+            obj.Director = dn;
+            obj.NumberofStudents = no;
+
+            //Console.WriteLine(obj.Name);
+
+        }
+       public void Init_Comp(string n, string dn, int no)
+        {
+            CompDep obj = new CompDep();
+
+            obj.Name = n;
+            obj.Director = dn;
+            obj.Numberofcomp = no;
+
+        }  */
+    }
+}
